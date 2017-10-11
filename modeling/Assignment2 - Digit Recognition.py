880d98fa972688a35424d4651866bf4bb8b2587f
David W Grossman <2gotgrossman@gmail.com>
2017-10-10 22:21:47 -0400

    updated generated python script from jupyter notebook

--- modeling/Assignment2 - Digit Recognition.py
+++ modeling/Assignment2 - Digit Recognition.py
@@ -1,13 +1,16 @@
 
 # coding: utf-8
 
-# In[64]:
+# In[1]:
 
 import numpy as np
-from statistics import mode
+from collections import Counter  # For mode
+from sklearn.metrics import confusion_matrix
 
 
-# In[39]:
+# #### Load data
+
+# In[2]:
 
 # lines cotains a list of a list of values. The first value in each sub-list is the y_value
 def get_formatted_data(data):
@@ -18,9 +21,11 @@ def get_formatted_data(data):
     for line in lines:
         y_vals.append(int(float(line[0])))
         x_vals.append( tuple( map(float, line[1:-1]) ) )
-    print(len(y_vals))
+    print len(y_vals)
     return [x_vals, y_vals]
 
+
+# In[3]:
 
 with open("zip.train", 'r') as f:
     train_data = f.read()
@@ -28,52 +33,61 @@ with open("zip.test", 'r') as f:
     test_data = f.read()
 train_x_data, train_y_data = get_formatted_data(train_data)
 test_x_data, test_y_data = get_formatted_data(test_data)
+print "Observations in training set: ", len(train_y_data)
+print "Observations in testing set: ", len(test_y_data)
 
 
-# In[46]:
+# #### Some Common Functions
+# **_Note: since we can have more than 1 mode, we will just take the first mode we find._**
+
+# In[6]:
 
 def get_distance_metric(vec1, vec2):
-    diffs = map(lambda x, y: (x-y)**2, zip(vec1, vec2))
+    diffs = map(lambda (x,y): (x-y)**2, zip(vec1, vec2))
     return sum(diffs)
 
+def get_mode(arr):
+    return Counter(arr).most_common(1)[0][0]
 
-# In[65]:
+
+# #### K Nearest Neighbors Algorithm
+
+# In[7]:
+
 def get_label_k_nn(test_vec, train_x_data, train_y_data, k=1):
-
     distances = map(lambda x: get_distance_metric(test_vec, x), train_x_data)
     
     # finds the indices of the k values that have minimal distance
     # Uses numpy magic
-    k = -k
-    min_indices = np.argpartition(np.array(distances), k)[k:]
-    min_indices = list(min_indices)
-
+    min_indices = np.argpartition(np.array(distances), k)[:k]
     labels = map(lambda x: train_y_data[x], min_indices)
     # Gets the mode
-    mode(labels)
+    return get_mode(labels)
 
 
-# In[70]:
+# In[8]:
 
 def test_knn(train_x_data, train_y_data, test_x_data, test_y_data, k=1):
     correct = 0
     for test_vec, test_label, i in zip(test_x_data, test_y_data, range(len(test_y_data))):
         prediction = get_label_k_nn(test_vec, train_x_data, train_y_data, k)
         if test_label == prediction:
-            print("Correct")
             correct += 1
-        else:
-            print("False")
     return float(correct) / len(test_y_data)
 
 
-# In[71]:
+# #### Test for K=1
+# It takes about .5 seconds per test example (15 minutes total). The expensive part is generating the `distances` vector.
+
+# In[163]:
 
 test_knn(train_x_data, train_y_data, test_x_data, test_y_data, 1)
 
 
-import sys
-sys.exit(1)
+# #### Since it takes so long to do this K-NN, we're going to try something slightly different. 
+# We're going to create a sorted list of the indices of the distances for each test observation. Then we can find the optimal K pretty quickly
+
+# In[9]:
 
 def get_sorted_distance_vec(test_vec, train_x_data, train_y_data):
     distances = map(lambda x: get_distance_metric(test_vec, x), train_x_data)
@@ -82,7 +96,7 @@ def get_sorted_distance_vec(test_vec, train_x_data, train_y_data):
     return x
 
 
-# In[101]:
+# In[10]:
 
 def get_distances_for_all(train_x_data, train_y_data, test_x_data, test_y_data):
     correct = 0
@@ -90,30 +104,50 @@ def get_distances_for_all(train_x_data, train_y_data, test_x_data, test_y_data):
     for test_vec, test_label, i in zip(test_x_data, test_y_data, range(len(test_y_data))):
         prediction_vec = get_sorted_distance_vec(test_vec, train_x_data, train_y_data)
         distance_vectors.append(prediction_vec)
-        if i % 10 ==0:
-            print(i)
     return distance_vectors
+
+
+# In[ ]:
 
 distance_vectors = get_distances_for_all(train_x_data, train_y_data, test_x_data, test_y_data)
 
+
+# #### For the first 100 values of K, get K-NN
+
 # In[ ]:
 
-for k in range(1, 101)[:1]:
-    k_scores = {}
+k_scores = []
+for k in range(1, 101):
+    correct = 0
     for i, (vec, true_label) in enumerate(zip(distance_vectors, test_y_data)):
+        # Takes only the first k values and finds their corresponding labels
         labels = map(lambda x: train_y_data[x], vec[:k])
-        prediction = mode(labels)
-        correct = 0
+        prediction = get_mode(labels)
         if prediction == true_label:
             correct += 1
-            
-    k_scores[k] = float(correct) / float(len(test_y_data))     
-print(k_scores)
+    k_scores.append(float(correct) / float(len(test_y_data)))
+print k_scores
 
+
+# #### And the best K is....
 
 # In[ ]:
 
+print "Max K =", np.argmax(k_scores) + 1, "with score of", max(k_scores)
 
+
+# #### Now, let's find the Confusion Matrix
+
+# In[ ]:
+
+correct = 0
+K = 3
+predictions = []
+for i, (vec, true_label) in enumerate(zip(distance_vectors, test_y_data)):
+    labels = map(lambda x: train_y_data[x], vec[:k])
+    prediction = get_mode(labels)
+    predictions.append(prediction)
+confusion_matrix(test_y_data, predictions, labels=range(10))
 
 
 # In[ ]:
